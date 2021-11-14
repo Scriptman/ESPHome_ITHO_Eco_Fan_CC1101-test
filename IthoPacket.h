ab3dc8410d567684860ee23cb3e2248999cc598c
Mario Lamers <mlamers@assert-development.nl>
2021-11-14 12:30:59 +0100

    Updated IthoPacket

--- IthoPacket.h
+++ IthoPacket.h
@@ -82,53 +82,60 @@ static char const * const MsgType[4] = { "RQ", " W", " I", "RP" };
 
 class IthoPacket
 {
-  public:
-	enum Type : uint32_t {
-      BIND          = 0x1FC9,
-      LEVEL         = 0x22F1,
-      TIMER         = 0x22F3,
-      SETPOINT      = 0x22F8,
-      STATUS        = 0x31DA,
-      REMOTESTATUS  = 0x31E0,
-      TEMPHUM       = 0x12A0,
-      CO2           = 0x1298,
-      BATTERY       = 0x1060
+    public:
+        uint8_t deviceId1[6];
+		uint8_t deviceId2[8];
+		IthoMessageType messageType;
+		IthoCommand command;
+		IthoCommand previous;
+
+		uint8_t counter;		//0-255, counter is increased on every remote button press
+
+        //new
+        enum Type : uint32_t {
+        BIND          = 0x1FC9,
+        LEVEL         = 0x22F1,
+        TIMER         = 0x22F3,
+        SETPOINT      = 0x22F8,
+        STATUS        = 0x31DA,
+        REMOTESTATUS  = 0x31E0,
+        TEMPHUM       = 0x12A0,
+        CO2           = 0x1298,
+        BATTERY       = 0x1060
+        };
+
+        IthoCommand command;
+
+        int8_t state;
+
+        //Message Fields
+        //<HEADER> <addr0> <addr1> <addr2> <param0> <param1> <OPCODE> <LENGTH> <PAYLOAD> <CHECKSUM>
+        //<  1   > <  3  > <  3  > <  3  > <  1   > <  1   > <  2   > <  1   > <length > <   1    >
+
+        uint8_t header;
+        uint8_t type;
+        uint32_t deviceId0;
+        uint32_t deviceId1;
+        uint32_t deviceId2;
+        uint8_t param0;
+        uint8_t param1;
+        uint16_t opcode;
+        uint8_t len;
+
+        uint8_t error;
+
+        uint8_t payloadPos;
+        uint8_t payload[MAX_PAYLOAD];
+
+        // Todo: remove
+        IthoMessageType messageType;
+        IthoCommand previous;
+        // end todo
+
+        uint8_t dataDecoded[MAX_DECODED];
+        uint8_t dataDecodedChk[MAX_DECODED];
+        uint8_t length;
+
+        uint8_t deviceType;
+        uint8_t deviceId[3];
     };
-
-	IthoCommand command;
-
-    int8_t state;
-
-	//Message Fields
-    //<HEADER> <addr0> <addr1> <addr2> <param0> <param1> <OPCODE> <LENGTH> <PAYLOAD> <CHECKSUM>
-    //<  1   > <  3  > <  3  > <  3  > <  1   > <  1   > <  2   > <  1   > <length > <   1    >
-
-    uint8_t header;
-    uint8_t type;
-    uint32_t deviceId0;
-    uint32_t deviceId1;
-    uint32_t deviceId2;
-    uint8_t param0;
-    uint8_t param1;
-    uint16_t opcode;
-    uint8_t len;
-
-	uint8_t error;
-
-    uint8_t payloadPos;
-    uint8_t payload[MAX_PAYLOAD];
-
-    // Todo: remove
-	IthoMessageType messageType;
-	IthoCommand previous;
-	// end todo
-
-    uint8_t dataDecoded[MAX_DECODED];
-    uint8_t dataDecodedChk[MAX_DECODED];
-	uint8_t length;
-
-	uint8_t deviceType;
-    uint8_t deviceId[3];
-
-	uint8_t counter;		//0-255, counter is increased on every remote button press
-};
